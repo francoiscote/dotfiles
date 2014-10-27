500a9896afab4d1243784d4d9b71150d40163640
François C <francois@ecorce.ca>
2014-10-27 18:59:38 -0400

    aliases: ga for git-add and wtf for git-wtf

--- oh-my-zsh/custom/plugins/fcote/fcote.plugin.zsh
+++ oh-my-zsh/custom/plugins/fcote/fcote.plugin.zsh
@@ -17,6 +17,9 @@ alias gl='git pull --prune'
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias ga='git-all'
+alias wtf='git-wtf'
+
 
 
 # flushdns, because I can never remember the full thing
