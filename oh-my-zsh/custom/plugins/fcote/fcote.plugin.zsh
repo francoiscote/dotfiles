678f2198485b3dec6ea8001ce5d12357b69adf56
François Côté <francois@francoiscote.net>
2015-06-25 11:39:41 -0400

    glog: use no pager

--- oh-my-zsh/custom/plugins/fcote/fcote.plugin.zsh
+++ oh-my-zsh/custom/plugins/fcote/fcote.plugin.zsh
@@ -16,7 +16,7 @@ alias gs='git status -sb'
 alias gl='git pull'
 alias gp='git push'
 alias gd='git diff'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git --no-pager log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias ga='git-all'
 alias wtf='git-wtf'
 
