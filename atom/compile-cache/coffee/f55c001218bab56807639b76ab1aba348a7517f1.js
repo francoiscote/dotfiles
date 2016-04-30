16cfe4cf8a48963e1efd72e2f7bcfdc39434d5ef
François Côté <francois@francoiscote.net>
2016-04-30 11:52:18 -0400

    remove atom storage

--- atom/compile-cache/coffee/f55c001218bab56807639b76ab1aba348a7517f1.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL2Zjb3RlLy5kb3RmaWxlcy9hdG9tL2luaXQuY29mZmVlIgogIF0sCiAgIm5hbWVzIjogW10sCiAgIm1hcHBpbmdzIjogIkFBQUE7QUFBQTtBQUFBIgp9
-
-//# sourceURL=/Users/fcote/.dotfiles/atom/init.coffee
