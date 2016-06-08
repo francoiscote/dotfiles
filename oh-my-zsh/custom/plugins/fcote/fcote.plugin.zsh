b3c5f73a36cf860b6670c2c275cc21a8716d9433
François Côté <francois@francoiscote.net>
2016-06-08 14:37:35 -0400

    tons of changes again

--- oh-my-zsh/custom/plugins/fcote/fcote.plugin.zsh
+++ oh-my-zsh/custom/plugins/fcote/fcote.plugin.zsh
@@ -31,7 +31,7 @@ alias rockrun80dev="sudo HTTP_PORT=80 rock --env dev run"
 alias rcrb="rock clean; rock build"
 
 # docker rock
-# alias rocker="docker run --rm -i -t -v $(pwd):/code -w /code rockstack:latest /bin/bash"
+alias rocker='docker run --rm --net=host -i -t -v $(pwd):/code -w /code rockstack:latest /bin/bash'
 
 # Source my ZSH
 alias reload!="source ~/.zshrc";
