aa59ed2b7a5c2a4cb019c5dd216744f3c4913c00
François Côté <francois@francoiscote.net>
2017-05-19 09:28:40 -0400

    hyper.js configs.

--- hyper.js
+++ hyper.js
@@ -5,13 +5,14 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 15,
 
     // font family with optional fallbacks
     fontFamily: '"Operator Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    uiFontFamily: '"Operator Mono", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: '#535860',
+    cursorColor: '#5C6370',
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'UNDERLINE',
@@ -20,20 +21,19 @@ module.exports = {
     cursorBlink: true,
 
     // color of the text
-    foregroundColor: '#8c97a9',
+    foregroundColor: '#abb2bf',
 
     // terminal background color
-    backgroundColor: '#1f2126',
+    backgroundColor: '#282c34',
 
     // border color (window, tabs)
-    borderColor: '#505864',
+    borderColor: '#333',
 
     // custom css to embed in the main window
     css: '',
 
     // custom css to embed in the terminal window
-    termCSS: `
-    `,
+    termCSS: '',
 
     // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
     // default: `false` on Linux, `true` on Windows (ignored on macOS)
@@ -42,31 +42,31 @@ module.exports = {
     // set to `false` if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
+    showWindowControls: 'false',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: '7px 10px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#bf6e7c',
-      green: '#88b379',
-      yellow: '#d9bd86',
-      blue: '#59b3c5',
-      magenta: '#b27ecd',
-      cyan: '#59b3c5',
-      white: '#59b3c5',
-      lightBlack: '#535860',
-      lightRed: '#bf6e7c',
-      lightGreen: '#88b379',
-      lightYellow: '#d9bd86',
-      lightBlue: '#59b3c5',
-      lightMagenta: '#b27ecd',
-      lightCyan: '#59b3c5',
-      lightWhite: '#e5e5e5'
+      black: '#0f1011',
+      red: '#e06c75',
+      green: '#98c379',
+      yellow: '#e5c07b',
+      blue: '#56b6c2',
+      magenta: '#c678dd',
+      cyan: '#61afef',
+      white: '#3e4451',
+      lightBlack: '#5C6370',
+      lightRed: '#e06c75',
+      lightGreen: '#98c379',
+      lightYellow: '#e5c07b',
+      lightBlue: '#56b6c2',
+      lightMagenta: '#c678dd',
+      lightCyan: '#61afef',
+      lightWhite: '#ffffff'
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -84,7 +84,7 @@ module.exports = {
     env: {},
 
     // set to false for no bell
-    bell: false,
+    bell: 'SOUND',
 
     // if true, selected text will automatically be copied to the clipboard
     copyOnSelect: false
@@ -97,6 +97,7 @@ module.exports = {
     // bellSoundURL: 'http://example.com/bell.mp3',
 
     // for advanced config flags please refer to https://hyper.is/#cfg
+
   },
 
   // a list of plugins to fetch and install from npm
@@ -105,7 +106,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperlinks',
+    // 'hyperminimal'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
