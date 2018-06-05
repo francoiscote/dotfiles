c60ac50f9339ea24fb4351c0b1dcfe9d88776b5e
François Côté <francois@francoiscote.net>
2018-06-05 13:35:38 -0400

    fix macOS

---
+++ macOs/.scripts/defaults.sh
@@ -0,0 +1,5 @@
+# Font Smoothing, set to Thin for iterm and Atom
+# reference: https://github.com/kevinSuttle/macOS-Defaults/issues/17
+
+defaults write com.googlecode.iterm2 AppleFontSmoothing -int 1
+defaults write com.github.atom AppleFontSmoothing -int 1
