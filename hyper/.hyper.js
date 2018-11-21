2b4623f67d9c685825b9820a1f6f5dc2cc5444ad
François Côté <francois@francoiscote.net>
2018-11-21 08:17:51 -0500

    format hyper.js

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -6,34 +6,35 @@ module.exports = {
   config: {
     // Choose either "stable" for receiving highly polished,
     // or "canary" for less polished but more frequent updates
-    updateChannel: 'stable',
+    updateChannel: "stable",
 
     // default font size in pixels for all tabs
-    fontSize: 15,
+    fontSize: 13,
 
     // font family with optional fallbacks
-    fontFamily: '"Operator Mono SSm Lig", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily:
+      '"Operator Mono SSm Lig", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: '#5F6B85',
+    cursorColor: "#5F6B85",
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'UNDERLINE',
+    cursorShape: "UNDERLINE",
 
     // set to true for blinking cursor
     cursorBlink: true,
 
     // color of the text
-    foregroundColor: '#ABB2BF',
+    foregroundColor: "#ABB2BF",
 
     // terminal background color
-    backgroundColor: '#282c34',
+    backgroundColor: "#282c34",
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: "#333",
 
     // custom css to embed in the main window
-    css: '',
+    css: "",
 
     // custom css to embed in the terminal window
     termCSS: `
@@ -49,36 +50,36 @@ module.exports = {
 
     // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
     // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
+    showHamburgerMenu: "",
 
     // set to `false` if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
+    showWindowControls: "",
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: "12px 14px",
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#E06C75',
-      green: '#98C379',
-      yellow: '#E5C07B',
-      blue: '#56B6C2',
-      magenta: '#C678DD',
-      cyan: '#61AFEF',
-      white: '#FFFFFF',
-      lightBlack: '#5F6B85',
-      lightRed: '#E06C75',
-      lightGreen: '#98C379',
-      lightYellow: '#E5C07B',
-      lightBlue: '#56B6C2',
-      lightMagenta: '#C678DD',
-      lightCyan: '#61AFEF',
-      lightWhite: '#FFFFFF'
+      black: "#000000",
+      red: "#E06C75",
+      green: "#98C379",
+      yellow: "#E5C07B",
+      blue: "#56B6C2",
+      magenta: "#C678DD",
+      cyan: "#61AFEF",
+      white: "#FFFFFF",
+      lightBlack: "#5F6B85",
+      lightRed: "#E06C75",
+      lightGreen: "#98C379",
+      lightYellow: "#E5C07B",
+      lightBlue: "#56B6C2",
+      lightMagenta: "#C678DD",
+      lightCyan: "#61AFEF",
+      lightWhite: "#FFFFFF"
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -93,11 +94,11 @@ module.exports = {
     //
     // Powershell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '/usr/local/bin/zsh',
+    shell: "/usr/local/bin/zsh",
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
-    shellArgs: ['--login'],
+    shellArgs: ["--login"],
 
     // for environment variables
     env: {},
@@ -127,9 +128,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    'hyperlinks',
-  ],
+  plugins: ["hyperlinks"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
