dd11da2b735bff58370c3a1e7be3efd8ba8ae851
François Côté <francois@francoiscote.net>
2019-06-16 08:33:41 -0400

    updates

--- zsh/lib/version-managers.zsh
+++ zsh/lib/version-managers.zsh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 # -----------------------------------------
+# NVM
+# -----------------------------------------
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# -----------------------------------------
 # PHPBREW
 # -----------------------------------------
 [[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
@@ -8,6 +15,8 @@
 # -----------------------------------------
 # PYENV
 # -----------------------------------------
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 if which pyenv > /dev/null;  then
   eval "$(pyenv init -)"
 fi
