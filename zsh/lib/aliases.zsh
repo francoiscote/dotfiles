a3371678cded57b52ef86d5bdec0059d9cd6ba2a
François Côté <francois@francoiscote.net>
2019-11-29 10:26:52 -0500

    tmux and tmuxp updates

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -9,10 +9,13 @@ alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pa
 # Source my ZSH
 alias reload!="source ~/.zshrc";
 
+# TMUX
 # Load tmuxp in current folder
 alias tt="tmuxp load .tmuxp.yaml"
 # list tmux sessions
 alias tl="tmux list-sessions"
+# kill server
+alias tk="tmux kill-server"
 
 # Docker
 alias dockerprune="docker image prune -af && docker volume prune -f"
