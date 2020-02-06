12d3aec4a3b9eab3944c8c37a1a457fc64f65384
François Côté <francois@francoiscote.net>
2020-02-06 15:53:00 -0500

    use n instead of nvm

--- zsh/lib/version-managers.zsh
+++ zsh/lib/version-managers.zsh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # -----------------------------------------
-# NVM
+# n
 # -----------------------------------------
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export N_PREFIX="$HOME/.n"
+export PATH=$N_PREFIX/bin:$PATH
 
 # -----------------------------------------
 # PHPBREW
