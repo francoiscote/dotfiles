6774c4e549ae06246e70185525fa580327650cfc
François Côté <francois@francoiscote.net>
2020-03-09 15:04:49 -0400

    hammerspoon: moveResize

--- hammerspoon/.hammerspoon/hyper.lua
+++ hammerspoon/.hammerspoon/hyper.lua
@@ -1,15 +1,19 @@
 local modal = hs.hotkey.modal.new({}, "F17")
+local moveResize = require("moveResize")
 
 -- Enter Hyper Modal when HyperKey is pressed
 local pressHyper = function()
   modal.triggered = false
   modal:enter()
+
+  moveResize.start()
 end
 
 -- Leave Hyper Modal when Hyper key is pressed,
 --   send ESCAPE if no other keys are pressed.
 local releaseHyper = function()
   modal:exit()
+  moveResize.stop()
   if not modal.triggered then
     hs.eventtap.keyStroke({}, 'ESCAPE')
   end
