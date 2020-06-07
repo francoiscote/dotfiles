70891227f2c1ea92ad88e708f0923e40f6008c2a
François Côté <francois@francoiscote.net>
2020-06-07 14:19:45 -0400

    tcl-tk

--- zsh/lib/version-managers.zsh
+++ zsh/lib/version-managers.zsh
@@ -16,6 +16,15 @@ export PATH=$N_PREFIX/bin:$PATH
 # -----------------------------------------
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+
+# tcl-tk
+export PATH="$(brew --prefix tcl-tk)/bin:$PATH"
+export LDFLAGS="-L$(brew --prefix tcl-tk)/lib"
+export CPPFLAGS="-I$(brew --prefix tcl-tk)/include"
+export PKG_CONFIG_PATH="$(brew --prefix tcl-tk)/lib/pkgconfig"
+export CFLAGS="-I$(brew --prefix tcl-tk)/include"
+export PYTHON_CONFIGURE_OPTS="--with-tcltk-includes='-I$(brew --prefix tcl-tk)/include' --with-tcltk-libs='-L$(brew --prefix tcl-tk)/lib -ltcl8.6 -ltk8.6'"
+
 if which pyenv > /dev/null;  then
   eval "$(pyenv init -)"
 fi
