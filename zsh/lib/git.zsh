47e2d584f09aa76361c18d525730df3805d18c8c
François Côté <francois@francoiscote.net>
2020-10-01 11:22:22 -0400

    git aliases (because no more oh-my-zsh)

--- zsh/lib/git.zsh
+++ zsh/lib/git.zsh
@@ -2,5 +2,29 @@
 # -----------------------------------------
 # ALIASES
 # -----------------------------------------
+alias gaa='git add --all'
+
+alias gb='git branch'
+alias gbd='git branch -d'
+
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gcam='git commit -a -m'
+
+alias gcb='git checkout -b'
+alias gco='git checkout'
+
+alias gd='git diff'
+
+alias gf='git fetch'
+alias gfa='git fetch --all --prune'
+
+alias gl='git pull'
+
+alias glog='git log --oneline --decorate --graph'
+alias gloga='git log --oneline --decorate --graph --all'
+
+alias gp='git push'
+
 alias gs='git status -sb'
 alias wtf='git-wtf'
