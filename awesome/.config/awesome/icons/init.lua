5f22aa85045446d92fb1041a42393d5d24c4ae46
François Côté <francois@francoiscote.net>
2021-05-13 11:09:00 -0400

    remove awesome

--- awesome/.config/awesome/icons/init.lua
+++
@@ -1,8 +0,0 @@
--- define icon folder directory
-local dir = os.getenv('HOME') .. '/.config/awesome/icons/'
-
--- return icons
-return {
-  leftarrow = dir .. 'left-arrow.svg',
-  rightarrow = dir .. 'right-arrow.svg'
-}
