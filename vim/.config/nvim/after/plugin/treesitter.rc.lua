c29ce906bbf775de07bdc2f1dc3de65d37f297c4
François Côté <francois@francoiscote.net>
2021-11-24 17:13:11 -0500

    treesiter: add lua

--- vim/.config/nvim/after/plugin/treesitter.rc.lua
+++ vim/.config/nvim/after/plugin/treesitter.rc.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-	ensure_installed = {'javascript', 'typescript', 'tsx', 'vim'},
+	ensure_installed = {'javascript', 'typescript', 'tsx', 'vim', 'lua'},
 	highlight = {
 		enable = true
 	},
