e13a5f5fc8916ff383b2e6835c41fef40bed6b36
François Côté <francois@francoiscote.net>
2023-08-08 22:16:30 -0400

    new audio setup with AUKEY bluetooth

--- hammerspoon/.hammerspoon/audio.lua
+++ hammerspoon/.hammerspoon/audio.lua
@@ -1,30 +1,38 @@
 local hyper = {"cmd", "alt", "ctrl"}
 
--- Swap between a Main audio output and a series of ranked secondary outputs
--- Main Output: Vanatoo T0 or Studio Display Speakers
--- Ranked Secondary Outputs:x
---   1) EVO4
---   2) External Headphones
+-- Swap between a Speaker output and a series of ranked secondary outputs (headphones)
+-- Main Output: Vanatoo T0 or Studio Display Speakers or Bluetooth connected Stereo system
+-- Ranked Secondary Headphones: EVO4 or External Headphones or Macbook Pro Speakers
 -------------------------------------------------------------------------------
 hs.hotkey.bind(hyper, "s", function()
   local currentDeviceName = hs.audiodevice.defaultOutputDevice():name()
   local nextDevice
-  if string.find(currentDeviceName, 'Vanatoo T0') or string.find(currentDeviceName, 'Studio Display Speakers') then
+  if string.find(currentDeviceName, 'Vanatoo T0') or string.find(currentDeviceName, 'Studio Display Speakers') or string.find(currentDeviceName, 'AUKEY') then
     nextDevice = hs.audiodevice.findOutputByName('External Headphones')
     if (nextDevice == nil) then
       nextDevice = hs.audiodevice.findOutputByName('EVO4')
     end
+    if (nextDevice == nil) then
+      nextDevice = hs.audiodevice.findOutputByName('MacBook Pro Speakers')
+    end
   else
-    nextDevice = hs.audiodevice.findOutputByName('Vanatoo T0')
+    nextDevice = hs.audiodevice.findOutputByName('AUKEY BR-C1')
+    if (nextDevice == nil) then
+      nextDevice = hs.audiodevice.findOutputByName('Vanatoo T0')
+    end
     if (nextDevice == nil) then
       nextDevice = hs.audiodevice.findOutputByName('Studio Display Speakers')
     end
   end
 
-  local didChange = nextDevice:setDefaultOutputDevice()
-  -- Also set the default Effect device because of a bug where it would change to something random when using setDefaultOutputDevice()
-  -- it can't keep the "Selected Sound Output Device" setting.
-  nextDevice:setDefaultEffectDevice()
+  local didChange = false
+  if (nextDevice ~= nil) then
+    didChange = nextDevice:setDefaultOutputDevice()
+    -- Also set the default Effect device because of a bug where it would change to something random when using setDefaultOutputDevice()
+    -- it can't keep the "Selected Sound Output Device" setting.
+    nextDevice:setDefaultEffectDevice()
+    didChange = true;
+  end
 
   if (didChange == true) then
     hs.alert.closeAll()
