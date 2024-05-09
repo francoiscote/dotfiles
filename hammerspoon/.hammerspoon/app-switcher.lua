28a6daff2f17280daa47fe3fcc81a3f8dcff8e47
François Côté <francois@francoiscote.net>
2024-05-09 09:21:26 -0400

    kitty

--- hammerspoon/.hammerspoon/app-switcher.lua
+++ hammerspoon/.hammerspoon/app-switcher.lua
@@ -1,42 +1,42 @@
-local hyper = {"cmd", "alt", "ctrl"}
-local hyperShift = {"cmd", "alt", "ctrl", "shift"}
+local hyper = { "cmd", "alt", "ctrl" }
+local hyperShift = { "cmd", "alt", "ctrl", "shift" }
 
 local charToApps = {
   -- Top Row: IM + Spotify
-  {'t', 'Toggl Track'},
-  {'u', 'Slack'},
-  {'u', 'Discord', 'shift'},
-  {'i', 'Messages'},
-  {'i', 'WhatsApp', 'shift'},
-  {'o', 'Messenger'},
-  {'p', 'Spotify'},
+  { 't', 'Toggl Track' },
+  { 'u', 'Slack' },
+  { 'u', 'Discord',                   'shift' },
+  { 'i', 'Messages' },
+  { 'i', 'WhatsApp',                  'shift' },
+  { 'o', 'Messenger' },
+  { 'p', 'Spotify' },
 
   -- Middle Row: Main Apps
-  {'h', 'com.culturedcode.ThingsMac'},
-  {'j', 'Google Chrome'},
-  {'j', 'Firefox Developer Edition', 'shift'},
-  {'k', 'Visual Studio Code'},
-  {'l', 'iTerm'},
-  {';', 'Figma'},
-  
+  { 'h', 'com.culturedcode.ThingsMac' },
+  { 'j', 'Google Chrome' },
+  { 'j', 'Firefox Developer Edition', 'shift' },
+  { 'k', 'Visual Studio Code' },
+  { 'l', 'kitty' },
+  { ';', 'Figma' },
+
   -- Bottom Row: Email, Calendar and ToDos
-  {'b', 'Google Meet'},
-  {'b', 'zoom.us', 'shift'},
-  {'n', 'Obsidian'},
-  {'n', 'Notion', 'shift'},
-  {'m', 'Mail'},
-  {',', 'Fantastical'},
-  {'.', 'Finder'},
+  { 'b', 'Google Meet' },
+  { 'b', 'zoom.us',                   'shift' },
+  { 'n', 'Obsidian' },
+  { 'n', 'Notion',                    'shift' },
+  { 'm', 'Mail' },
+  { ',', 'Fantastical' },
+  { '.', 'Finder' },
 }
 
 for i, app in ipairs(charToApps) do
   local hyperKey = app[3] == 'shift' and hyperShift or hyper
   hs.hotkey.bind(hyperKey, app[1], function()
-      if (hs.application.launchOrFocus(app[2])) then
-        hyperKey.triggered = true
-      elseif (hs.application.launchOrFocusByBundleID(app[2])) then
-        hyperKey.triggered = true
-      end
+    if (hs.application.launchOrFocus(app[2])) then
+      hyperKey.triggered = true
+    elseif (hs.application.launchOrFocusByBundleID(app[2])) then
+      hyperKey.triggered = true
     end
+  end
   )
 end
