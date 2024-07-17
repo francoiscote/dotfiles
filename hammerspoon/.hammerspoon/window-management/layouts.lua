7f06471864c090e78dac16b6f90465e5cb269506
François Côté <francois@francoiscote.net>
2024-07-17 13:03:26 -0400

    fix layout

--- hammerspoon/.hammerspoon/window-management/layouts.lua
+++ hammerspoon/.hammerspoon/window-management/layouts.lua
@@ -9,13 +9,6 @@ local hsGrid = grid.hsGrid
 -------------------------------------------------------------------------------
 local wf = hs.window.filter
 local mainScreenName = hs.screen.mainScreen():name()
--- Single Apps
-local w_arc = wf.new { 'Arc' }:setCurrentSpace(true)
-local w_chrome = wf.new { 'Google Chrome' }:setCurrentSpace(true)
-local w_firefox = wf.new { 'Firefox Developer Edition' }:setCurrentSpace(true)
-local w_obs = wf.new { 'OBS' }:setCurrentSpace(true)
-local w_figma = wf.new { 'Figma' }:setCurrentSpace(true)
-
 -- Groups of Apps
 local w_browsers = wf.new { ['Arc'] = true, ['Safari'] = true, ['Firefox Developer Edition'] = true, ['Google Chrome'] = { rejectTitles = 'Picture in Picture' } }
     :setCurrentSpace(true):setScreens(mainScreenName)
@@ -23,10 +16,13 @@ local w_editors = wf.new { 'Code' }:setCurrentSpace(true):setScreens(mainScreenN
 local w_terminals = wf.new { 'iTerm2', 'kitty' }:setCurrentSpace(true):setScreens(mainScreenName)
 local w_videos = wf.new { ['YouTube'] = true, ['Twitch'] = true, ['Google Meet'] = true, ['zoom.us'] = true, ['VLC'] = true, ['Google Chrome'] = { allowTitles = 'Picture in Picture' }, ['OBS Studio'] = { allowTitles = '(.*)Windowed Projector(.*)' }, ['Arc'] = { allowRoles = 'AXSystemDialog' }, ['Slack'] = { allowTitles = '(.*)Huddle$' }, ['Oryx'] = true }
     :setCurrentSpace(true):setScreens(mainScreenName)
-local w_notes = wf.new { 'Notion', 'Obsidian', 'Bear' }:setCurrentSpace(true):setScreens(mainScreenName)
+local w_cam = wf.new { ['Google Meet'] = true, ['zoom.us'] = true, ['OBS Studio'] = { allowTitles = '(.*)Windowed Projector(.*)' }, ['Slack'] = { allowTitles = '(.*)Huddle$' } }
+local w_notes = wf.new { 'Notion', 'Obsdian', 'Bear' }:setCurrentSpace(true):setScreens(mainScreenName)
 local w_todos = wf.new { 'Todoist', 'Things' }:setCurrentSpace(true):setScreens(mainScreenName)
 local w_chats = wf.new { 'Slack', 'WhatsApp', 'Discord', 'Messages', 'Messenger' }:setCurrentSpace(true):setScreens(
   mainScreenName)
+-- Single Apps
+local w_figma = wf.new { 'Figma' }:setCurrentSpace(true)
 local w_obs = wf.new { 'OBS Studio' }:setCurrentSpace(true):setScreens(mainScreenName)
 local w_twitch = wf.new { 'Twitch Dashboard' }:setCurrentSpace(true):setScreens(mainScreenName)
 
@@ -40,10 +36,16 @@ workBrowse:
 ]]
 function export.workBrowse(inverted)
   local layout
+
+  -- Force inverted if camera is on
+  if #w_cam:getWindows() > 0 then
+    inverted = true
+  end
+
   if inverted then
-    layout = areas.smallSplit
-  else
     layout = areas.smallSplitInverted
+  else
+    layout = areas.smallSplit
   end
 
   --MAIN
@@ -76,20 +78,26 @@ workCode:
 ]]
 function export.workCode(inverted)
   local layout
+
+  -- Force inverted if camera is on
+  if #w_cam:getWindows() > 0 then
+    inverted = true
+  end
+
   if inverted then
-    layout = areas.mediumSplit
-  else
     layout = areas.mediumSplitInverted
+  else
+    layout = areas.mediumSplit
   end
 
   -- MAIN
   grid.setFilteredWindowsToCell(w_editors, layout.main)
   grid.setFilteredWindowsToCell(w_figma, layout.main)
+  grid.setFilteredWindowsToCell(w_terminals, layout.main)
 
   if #w_videos:getWindows() > 0 then
     -- SECONDARY
     grid.setFilteredWindowsToCell(w_browsers, layout.secondaryBottom)
-    grid.setFilteredWindowsToCell(w_terminals, layout.secondaryBottom)
     grid.setFilteredWindowsToCell(w_notes, layout.secondaryBottom)
     grid.setFilteredWindowsToCell(w_todos, layout.secondaryBottom)
     grid.setFilteredWindowsToCell(w_chats, layout.secondaryBottom)
@@ -98,7 +106,6 @@ function export.workCode(inverted)
   else
     -- SECONDARY
     grid.setFilteredWindowsToCell(w_browsers, layout.secondaryFull)
-    grid.setFilteredWindowsToCell(w_terminals, layout.secondaryFull)
     grid.setFilteredWindowsToCell(w_notes, layout.secondaryFull)
     grid.setFilteredWindowsToCell(w_todos, layout.secondaryFull)
     grid.setFilteredWindowsToCell(w_chats, layout.secondaryFull)
@@ -107,72 +114,17 @@ function export.workCode(inverted)
 end
 
 --[[
-workEven:
-  When Video, place it on the left and everything else on the right
-  When no video, browser, notes and chats on the left, everything else on the right
-]]
-function export.workEven(inverted)
-  if (inverted == true) then
-    if #w_videos:getWindows() > 0 then
-      --LEFT
-      grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_notes, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_todos, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_figma, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_chats, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_browsers, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_editors, areas.evenSplit.leftFull)
-      -- RIGHT
-      grid.setFilteredWindowsToCell(w_videos, areas.evenSplit.rightFull)
-    else
-      -- RIGHT
-      grid.setFilteredWindowsToCell(w_browsers, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_todos, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_chats, areas.evenSplit.rightFull)
-      -- LEFT
-      grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_notes, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_figma, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_editors, areas.evenSplit.leftFull)
-    end
-  else
-    if #w_videos:getWindows() > 0 then
-      --RIGHT
-      grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_notes, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_todos, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_figma, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_chats, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_browsers, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_editors, areas.evenSplit.rightFull)
-      -- LEFT
-      grid.setFilteredWindowsToCell(w_videos, areas.evenSplit.leftFull)
-    else
-      -- LEFT
-      grid.setFilteredWindowsToCell(w_browsers, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_todos, areas.evenSplit.leftFull)
-      grid.setFilteredWindowsToCell(w_chats, areas.evenSplit.leftFull)
-      -- RIGHT
-      grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_notes, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_figma, areas.evenSplit.rightFull)
-      grid.setFilteredWindowsToCell(w_editors, areas.evenSplit.rightFull)
-    end
-  end
-end
-
---[[
-notesEven:
-  notes on the right, everything else on the left
-]]
+workEvnet:
+notes on the right, everything else on the left
+  ]]
 function export.workEven(mainNotes)
   if (mainNotes == true) then
     -- LEFT
-    grid.setFilteredWindowsToCell(w_editors, areas.evenSplit.leftFull)
     grid.setFilteredWindowsToCell(w_browsers, areas.evenSplit.leftFull)
+    grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.leftFull)
+    grid.setFilteredWindowsToCell(w_editors, areas.evenSplit.leftFull)
     grid.setFilteredWindowsToCell(w_todos, areas.evenSplit.leftFull)
     grid.setFilteredWindowsToCell(w_chats, areas.evenSplit.leftFull)
-    grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.leftFull)
     grid.setFilteredWindowsToCell(w_figma, areas.evenSplit.leftFull)
     -- RIGHT
     grid.setFilteredWindowsToCell(w_notes, areas.evenSplit.rightFull)
@@ -182,9 +134,9 @@ function export.workEven(mainNotes)
     grid.setFilteredWindowsToCell(w_browsers, areas.evenSplit.leftFull)
     grid.setFilteredWindowsToCell(w_todos, areas.evenSplit.leftFull)
     grid.setFilteredWindowsToCell(w_chats, areas.evenSplit.leftFull)
-    grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.leftFull)
     grid.setFilteredWindowsToCell(w_figma, areas.evenSplit.leftFull)
     -- RIGHT
+    grid.setFilteredWindowsToCell(w_terminals, areas.evenSplit.rightFull)
     grid.setFilteredWindowsToCell(w_editors, areas.evenSplit.rightFull)
   end
 end
@@ -198,8 +150,8 @@ workMax:
 function export.workMax()
   helpers.maximiseFilteredWindows(w_editors)
   helpers.maximiseFilteredWindows(w_figma)
+  helpers.maximiseFilteredWindows(w_terminals)
   grid.setFilteredWindowsToCell(w_browsers, areas.custom.browser)
-  grid.setFilteredWindowsToCell(w_terminals, areas.custom.center)
   grid.setFilteredWindowsToCell(w_notes, areas.custom.finder)
   grid.setFilteredWindowsToCell(w_todos, areas.custom.finder)
   grid.setFilteredWindowsToCell(w_chats, areas.custom.center)
@@ -275,8 +227,7 @@ function export.twitchCodeEven(inverted)
   end
 end
 
-function export.twitchCodeSmall(inverted)
-  -- Hidden
+function export.twitchCodeSmall(inverted) -- Hidden
   twitchHiddenWindows()
 
   if (inverted) then
